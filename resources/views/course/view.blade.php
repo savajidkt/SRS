c8a7dfc49a39c92361980b7f4a88f4b8b73b8ccc
JJParejiya <jj.parejiya@gmail.com>
2023-08-03 17:49:56 +0530

    The profile icon is not working in view course.

--- resources/views/course/view.blade.php
+++ resources/views/course/view.blade.php
@@ -292,8 +292,7 @@
 
 @section('extra-script')
 
-    <script src="{{ asset('vendor/global/global.min.js') }}"></script>
-
+   
     <script src="{{ asset('js/quixnav-init.js') }}"></script>
 
     <script src="{{ asset('js/custom.min.js') }}"></script>
